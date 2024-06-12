50a8185f4bce3099cf85925aeeee3ee31efccbdb
kidjuniper <kidjuniper@yandex.ru>
2024-06-12 12:38:58 +0300

    better advertising screen

--- Sources/AdvertisingForYouLib/AdvertisingForYouLib.swift
+++ Sources/AdvertisingForYouLib/AdvertisingForYouLib.swift
@@ -4,40 +4,102 @@ import UIKit
 import WebKit
 
 public class AdWebViewController: UIViewController {
-
-    private var webView: WKWebView!
-    private var url: URL
-
+    
+    private var adWebView: WKWebView!
+    private var adUrl: URL
+    
+    public let netAlert = UIAlertController(title: "Connection problems :(",
+                                            message: "To continue, you should be online",
+                                            preferredStyle: .alert)
+    
+    private var topConstraint: NSLayoutConstraint?
+    
     public init(url: URL) {
-        self.url = url
+        self.adUrl = url
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    override public func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.navigationDelegate = self
-        view = webView
+    
+    override public var prefersStatusBarHidden: Bool {
+        return true
     }
-
+    
+    private func configWebView() -> WKWebViewConfiguration {
+        let configuration = WKWebViewConfiguration()
+        configuration.preferences.javaScriptEnabled = true
+        configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
+        return configuration
+    }
+    
+    private func setUp() {
+        view.backgroundColor = .black
+        
+        adWebView = WKWebView(frame: view.bounds, configuration: configWebView())
+        adWebView.translatesAutoresizingMaskIntoConstraints = false
+        adWebView.navigationDelegate = self
+        view.addSubview(adWebView)
+        
+        NSLayoutConstraint.activate([
+            adWebView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            adWebView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            adWebView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        topConstraint = adWebView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0)
+        topConstraint!.isActive = true
+        
+        let request = URLRequest(url: adUrl)
+        adWebView.load(request)
+    }
+    
+    func showNetPopUp() {
+        present(netAlert, animated: true, completion: nil)
+    }
+    
+    func hideNetPopUp() {
+        netAlert.dismiss(animated: true)
+    }
+    
+    func checkInternetConnection() {
+        let monitor = NWPathMonitor()
+        monitor.pathUpdateHandler = { path in
+            if path.status == .satisfied {
+                DispatchQueue.main.async {
+                    self.hideNetPopUp()
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.showNetPopUp()
+                }
+            }
+        }
+        let queue = DispatchQueue(label: "NetworkMonitor")
+        monitor.start(queue: queue)
+    }
+    
+    override public func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        super.didRotate(from: fromInterfaceOrientation)
+        topConstraint?.constant = self.view.frame.size.height > self.view.frame.size.width ? 70 : 0
+        view.updateConstraintsIfNeeded()
+    }
+    
     override public func viewDidLoad() {
         super.viewDidLoad()
-        let request = URLRequest(url: url)
-        webView.load(request)
+        setUp()
+        checkInternetConnection()
     }
 }
 
 extension AdWebViewController: WKNavigationDelegate {
     public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print("Finished loading: \(url)")
+        //        print("Finished loading: \(url)")
     }
-
+    
     public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-        print("Failed to load: \(url), error: \(error.localizedDescription)")
+        //        print("Failed to load: \(url), error: \(error.localizedDescription)")
     }
 }
 
@@ -55,7 +117,7 @@ public final class AdFetcher {
         let url = URL(string: url)!
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
-
+        
         let session: URLSession = {
             let session = URLSession(configuration: .default)
             session.configuration.timeoutIntervalForRequest = 10.0
@@ -70,12 +132,13 @@ public final class AdFetcher {
                 completion(.failure(.responseError))
                 return
             }
+            
             completion(.success(data))
         }
         task.resume()
     }
     
-    public func fetchRelevantAd(source: String, 
+    public func fetchRelevantAd(source: String,
                                 keyword: String,
                                 appsId: String,
                                 idfa: String,
@@ -110,26 +173,26 @@ public final class AdFetcher {
     }
     
     public func fetchAndPresentAd(from viewController: UIViewController,
-                                      source: String,
-                                      keyword: String,
-                                      appsId: String,
-                                      idfa: String,
-                                    extraIfo: String? = "",
-                                      completion: @escaping (Bool) -> Void) {
-            fetchRelevantAd(source: source, keyword: keyword, appsId: appsId, idfa: idfa, extraInfo: extraIfo) { [weak self] urlString in
-                
-                if !urlString.isEmpty, let url = URL(string: urlString) {
-                    DispatchQueue.main.async {
-                        let webViewController = AdWebViewController(url: url)
-                        webViewController.modalPresentationStyle = .fullScreen
-                        viewController.present(webViewController, animated: true, completion: nil)
-                        completion(true)
-                    }
-                } else {
-                    completion(false)
+                                  source: String,
+                                  keyword: String,
+                                  appsId: String,
+                                  idfa: String,
+                                  extraIfo: String? = "",
+                                  completion: @escaping (Bool) -> Void) {
+        fetchRelevantAd(source: source, keyword: keyword, appsId: appsId, idfa: idfa, extraInfo: extraIfo) { [weak self] urlString in
+            
+            if !urlString.isEmpty, let url = URL(string: urlString) {
+                DispatchQueue.main.async {
+                    let webViewController = AdWebViewController(url: url)
+                    webViewController.modalPresentationStyle = .fullScreen
+                    viewController.present(webViewController, animated: true, completion: nil)
+                    completion(true)
                 }
+            } else {
+                completion(false)
             }
         }
+    }
 }
 
 public enum AdvClientError: Error {
